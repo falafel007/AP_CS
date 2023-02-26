a32a9e443fd71ea61d11a2a98dfc632997359c31
falafel007 <felleisen@gmail.com>
2023-02-26 16:22:14 -0600

    Chapter 1.4

--- src/WateringDay.java
+++ src/WateringDay.java
@@ -1,3 +1,14 @@
 // 1.4.3
 public class WateringDay {
+    int howMuch;
+    String nextDay;
+
+    WateringDay(int howMuch, String nextDay) {
+        this.howMuch = howMuch;
+        this.nextDay = nextDay;
+    }
+
+    String alarm() {
+        return "Water on " + nextDay + " with " + this.howMuch;
+    }
 }
